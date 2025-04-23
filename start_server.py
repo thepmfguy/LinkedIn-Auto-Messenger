cbb77a8defc752e8265c22ead3ff28562cc49b82
Gaurav Singh Bisen <thepmfguy@gmail.com>
2025-04-23 15:04:02 +0530

    updated for token issue

---
+++ start_server.py
@@ -0,0 +1,25 @@
+import http.server
+import socketserver
+import webbrowser
+import os
+
+PORT = 8000
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        super().end_headers()
+
+def start_server():
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"Serving at http://localhost:{PORT}")
+        print("Press Ctrl+C to stop the server")
+        webbrowser.open(f'http://localhost:{PORT}')
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nShutting down server...")
+            httpd.server_close()
+
+if __name__ == "__main__":
+    start_server() 
